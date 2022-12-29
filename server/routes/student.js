bdb22b2193f2dfbb32cd7448c2b584b8a29169ae
Ashrafpathan2094 <minazpathan2094@gmail.com>
2022-12-29 18:29:21 +0530

    client side

--- server/routes/student.js
+++ server/routes/student.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { getStudents } from '../controllers/student.js';
+import { getStudents, createStudent } from '../controllers/student.js';
+
+import student from "../models/student.js"
 
 
 const router = express.Router();
 
 
 router.get('/',getStudents);
+router.get('/',createStudent);
 
 export default router;
